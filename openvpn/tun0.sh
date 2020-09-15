91886a22a7b950e03f9607bc9f958f6d181426f2
demo-god <64624556+demo-god@users.noreply.github.com>
2020-09-15 11:50:38 -0400

    Update tun0.sh

--- openvpn/tun0.sh
+++ openvpn/tun0.sh
@@ -1,28 +1,24 @@
-client
-dev tun0
-# optional
-local <local ip>
-pull-filter ignore redirect-gateway
-# protocol may be udp or tcp based on your vpn
-proto udp
-remote <vpn domain> <vpn port>
-route-noexec
-route-up tun0.sh
-lport 0
-resolv-retry infinite
-persist-key
-persist-tun
-cipher aes-128-cbc
-auth sha1
-tls-client
-remote-cert-tls server
-auth-user-pass login.conf
-auth-retry interact
-comp-lzo
-verb 5
-reneg-sec 0
-crl-verify <certificate file name>
-log-append tun0.log
-script-security 2
-ca <certificate file name>
-disable-occ
+#!/bin/sh
+
+# This script removes the default routes added by OpenVPN
+# so that the VPN tunnel may be accessed only by applications
+# which are bound to the VPN interface
+
+# Checks to see if there is an IP routing table named 'vpn', create if missing
+if [ $(cat /etc/iproute2/rt_tables | grep tun0 | wc -l) -eq 0 ]; then
+            echo "100     tun0" >> /etc/iproute2/rt_tables
+fi
+
+# Remove any previous routes in the 'vpn' routing table
+/bin/ip rule | sed -n 's/.*\(from[ \t]*[0-9\.]*\).*tun0/\1/p' | while read RULE
+do
+        /bin/ip rule del ${RULE}
+done
+
+# Add routes to the vpn routing table
+/bin/ip rule add from ${ifconfig_local} lookup tun0
+
+# Add the route to direct all traffic using the the vpn routing table to the tunX interface
+/bin/ip route add default via ${route_vpn_gateway} table tun0
+
+exit 0
